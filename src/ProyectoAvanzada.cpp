a0ddddf48238eb158df384292c2d5d8b70eb4d1c
¨Gaston <gastonaguilera@gmail.com>
2023-03-23 14:18:41 -0300

    Modificacion del saludo

--- src/ProyectoAvanzada.cpp
+++ src/ProyectoAvanzada.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Este es el prime cambio" << endl; // prints !!!Hello World!!!
 	return 0;
 }
