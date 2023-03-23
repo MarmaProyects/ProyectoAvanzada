1e3b98c5069a8dd8dc2f717c5dd059c8184e0cd6
¨Gaston <gastonaguilera@gmail.com>
2023-03-23 00:48:43 -0300

    Hello World

---
+++ src/ProyectoAvanzada.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ProyectoAvanzada.cpp
+// Author      : Gaston Aguilera - Franco Sancristobal - Angelo Festino
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
